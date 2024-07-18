67a885f17ec6e08f0580d34eb10089a3a93de0f6
Felipe Yajima Batista <felipe.yajimabatista@gmail.com>
2024-07-18 20:46:14 -0300

    first commit

---
+++ reservas/src/test/java/br/com/fiap/aluno/reservas/reservas/ReservasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.aluno.reservas.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
