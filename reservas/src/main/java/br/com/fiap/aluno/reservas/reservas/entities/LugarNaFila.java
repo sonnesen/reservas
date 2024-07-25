31702c8fc701e99b5c07cbbeebe9dcbb969a6f7b
Felipe Yajima Batista <44686645+felipeyajima@users.noreply.github.com>
2024-07-24 21:22:24 -0300

    Delete reservas/src/main/java/br/com/fiap/aluno/reservas/reservas/entities/LugarNaFila.java

--- reservas/src/main/java/br/com/fiap/aluno/reservas/reservas/entities/LugarNaFila.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.aluno.reservas.reservas.entities;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name="tb_fila")
-public class LugarNaFila {
-}
